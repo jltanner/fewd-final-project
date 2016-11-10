2a084691e22c3bac93a9e3806e0f037b6adc3fc5
Jamie Tanner <jamietanner@gmail.com>
2016-11-10 17:57:50 -0500

    Added responsive styles and a little JS fade in & out thingy

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-$(".hamburger").on("click", function() {
-	$("nav").slideToggle();
-});
-$(window).on("resize", function() {
-	if ($(window).width() >= 768) {
-		$("nav").removeAttr("style");
-	};
-});
+var image_href;
+var image_href_next;
+var image_href_prev;
+var lightbox;
 
-// LIGHTBOX TESTING
+// LIGHTBOX
 
 	$(".lightbox-trigger").on("click", function(e) {
 		e.preventDefault();
-		var image_href = $(this).attr("href");
-		var image_href_next = $(this).next().attr("href");
-		var image_href_prev = $(this).prev().attr("href");
+		image_href = $(this).attr("href");
+		image_href_next = $(this).next().attr("href");
+		image_href_prev = $(this).prev().attr("href");
 
 		//create HTML for lightbox window
-		var lightbox = 
+		lightbox = 
 		'<div id="lightbox">' +
 		'<i class="fa fa-times close"></i>' +
 			'<div id="content">' + 
@@ -28,40 +24,74 @@ $(window).on("resize", function() {
 
 		// insert and fade in
 		$(lightbox).hide().appendTo("body").fadeIn();
+		$(image_href).addClass(".in-view");
 
 		// click to close
-		$("#lightbox i.close").on("click", function() {
-			$("#lightbox").fadeToggle();
+		$("body").on("click", "#lightbox", ".close", function() {
+			$(lightbox).fadeOut();
 		});
 
-		// next & prev image functions
-		function nextImg() {
-			$("#content img").attr("src", image_href_next);
-		}
-		function prevImg() {
-			$("#content img").attr("src", image_href_prev);
-		}
-
-		// next & prev click events
 		$("#lightbox img").on("click", function(){
-			nextImg();
+		nextImg();
+		$("#content img").removeClass(".in-view");
 		});
-		$("#lightbox img").on("swipeleft", function(){
-			nextImg();
-		});
-		$("#lightbox img").on("swiperight", function(){
-			prevImg();
-		});
-
-		$("#lightbox i.next").on("click", function(){
-			nextImg();
-		});
-
-		$("#lightbox i.prev").on("click", function(){
-			prevImg();
-		});
-
 
 	});
 
 
+	
+
+	// next & prev image functions
+	function nextImg() {
+		$("#content img").attr("src", image_href_next).addClass(".in-view");
+	}
+	function prevImg() {
+		$("#content img").attr("src", image_href_prev);
+	}
+
+	// next & prev click events
+	$("#lightbox img").on("click", function(){
+		nextImg();
+		$("lightbox img").addClass("in-view");
+	});
+	$("#lightbox img").on("swipeleft", function(){
+		nextImg();
+	});
+	$("#lightbox img").on("swiperight", function(){
+		prevImg();
+	});
+
+	$("#lightbox i.next").on("click", function(){
+		nextImg();
+	});
+
+	$("#lightbox i.prev").on("click", function(){
+		prevImg();
+	});
+
+// HAMBURGER MENU
+$(".hamburger").on("click", function() {
+	$("nav").slideToggle();
+	$(".consume").css("display", "none");
+});
+$(window).on("resize", function() {
+	if ($(window).width() >= 768) {
+		$("nav").removeAttr("style");
+		$(".consume").removeAttr("style");
+	};
+});
+
+// CONSUME FADE-IN / -OUT
+$(document).ready(function() {
+	var consume = $("#consume-box");
+	$(consume).hide();
+		$(function () {
+	    setInterval(function () {
+	        consume.fadeIn(1000, function () {
+	            consume.fadeOut(1000, function () {
+	                consume.fadeIn(1000)
+	            });
+	        });
+	    }, 4000);
+	});
+})
