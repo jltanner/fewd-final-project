c40e38bfdd138601d7af2e4d1ff2704d6c052bb8
Jamie Tanner <jamietanner@gmail.com>
2016-11-15 18:37:15 -0500

    excerpts added, plus little updates

--- js/main.js
+++ js/main.js
@@ -2,90 +2,108 @@ var image_href;
 var image_href_next;
 var image_href_prev;
 var lightbox;
+var winW = $(window).width();
 
 var excerpt = $("a.lightbox-trigger").map(function() {
     return this.href;
 }).toArray();
 var page = 0;
-console.log(excerpt);
 
 // LIGHTBOX
+$(".lightbox-trigger").on("click", function(e) {
+	e.preventDefault();
+	image_href = $(this).attr("href");
+	image_href_next = $(this).next().attr("href");
+	image_href_prev = $(this).prev().attr("href");
 
-	$(".lightbox-trigger").on("click", function(e) {
-		e.preventDefault();
-		image_href = $(this).attr("href");
-		image_href_next = $(this).next().attr("href");
-		image_href_prev = $(this).prev().attr("href");
+	// create HTML for lightbox window
+	lightbox = 
+	'<div id="lightbox">' +
+		'<i class="fa fa-times close"></i>' +
+		'<div id="content">' + 
+			'<i class="fa fa-chevron-left prev"></i>' +
+			'<img src="' + excerpt[0] +'" />' +
+			'<i class="fa fa-chevron-right next"></i>'
+		'</div>' +	
+	'</div>';
 
-		// //create HTML for lightbox window
-		lightbox = 
-		'<div id="lightbox">' +
-			'<i class="fa fa-times close"></i>' +
-			'<div id="content">' + 
-				'<i class="fa fa-chevron-left prev"></i>' +
-				'<img src="' + excerpt[0] +'" />' +
-				'<i class="fa fa-chevron-right next"></i>'
-			'</div>' +	
-		'</div>';
+	// insert and fade in
+	$(lightbox).hide().appendTo("body").fadeIn();
+});
 
-		// insert and fade in
-		$(lightbox).hide().appendTo("body").fadeIn();
-
-	});
-
-	$("body").on("click", "i.close", function() {
-			$("body #lightbox").fadeOut();
-		});
+// close lightbox
+$("body").on("click", "i.close", function() {
+	$("body #lightbox").fadeOut();
+});
 
 // next & prev image functions
-	function next() {
-		$("#content img").attr("src", excerpt[page+1]);
-		if (page == excerpt.length - 1) {
-            page = 0;
-            $("#content img").attr("src", excerpt[page]);
-		}
-        else {
-            page++;
-        }
-	};
-
-	function prev() {
-		$("#content img").attr("src", excerpt[page-1]);
-		if (page == 0) {
-            page = excerpt.length - 1;
-            $("#content img").attr("src", excerpt[page]);
-		}
-        else {
-            page--;
-        }
-	};
+function next() {
+	$("#content img").attr("src", excerpt[page+1]);
+	if (page == excerpt.length - 1) {
+		page = 0;
+		$("#content img").attr("src", excerpt[page]);
+	}
+	else {
+		page++;
+	}
+};
+function prev() {
+	$("#content img").attr("src", excerpt[page-1]);
+	if (page == 0) {
+		page = excerpt.length - 1;
+		$("#content img").attr("src", excerpt[page]);
+	}
+	else {
+		page--;
+	}
+};
 
 // next & prev image click events
-	$("body").on("click", "#content img", function(){
-		next();
-	});
+$(document).ready(function() {
+	winW = $(window).width();
+	if (winW > 768) {
+		$("body").on("click", "#content img", function(){
+			next();
+		});
+		$("body").on("click", "i.next", function(){
+			next();
+		});
+		$("body").on("click", "i.prev", function(){
+			prev();
+		});
+	} else if (winW <= 768) {
+		$("body").on("swipeleft", "#content img", function() {
+			next();
+		});
+		$("body").on("swiperight", "#content img", function() {
+			prev();
+		});
+		$("body").on("tap", function() {
+			$("#lightbox i.close").slideToggle();
+		});
+		$("body").on("tap", "#lightbox i.close", function() {
+			$("body #lightbox").fadeOut();
+		});
+	}
+})
 
-	$("body").on("click", "i.next", function(){
+$(document).on("keydown", function(e) {
+	if (e.keyCode == 39) {  
 		next();
-	});
-
-	$("body").on("click", "i.prev", function(){
+	}
+	if(e.keyCode==37) {
 		prev();
-	});
+	}
+});
 
-	$(document).on("keydown", function(e) {
-		if (e.keyCode == 39) {  
-            next();
-        }
-        if(e.keyCode==37) {
-            prev();
-        }
-	});
-
-	$("body").on("swipeleft", "#content img", function() {
-		next();
-	});
-
+$(window).on("resize", function() {
+	winW = $(window).width();
+	if (winW <= 768) {
+		$("body #lightbox i").hide();
+	} else {
+		$("body #lightbox i").show();
+	}
+});
 
 // HAMBURGER MENU
 $(".hamburger").on("click", function() {
